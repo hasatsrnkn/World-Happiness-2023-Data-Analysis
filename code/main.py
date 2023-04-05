f7ba939fd5391cacb1ed5e90f99aee11b66dd3bc
Hasat Serinkan <hasat.serinkan@ug.bilkent.edu.tr>
2023-04-05 16:00:08 +0200

    first

--- code/main.py
+++ code/main.py
@@ -1,1 +1,13 @@
-## Main Python
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+df = pd.read_csv('data/WHR2023.csv')
+
+plt.plot(df['Ladder score'])
+plt.show()
+
+plt.plot(df['Social support'])
+plt.show()
